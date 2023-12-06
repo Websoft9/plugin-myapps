26ac57cc21fa48eb3a0a375a1f9e49c87122a510
chendelin1982 <chendelin1982@users.noreply.github.com>
2023-12-06 02:48:13 +0000

    Websoft9/plugin-myapps combine translation.json

--- public/po.zh_CN.js
+++ public/po.zh_CN.js
@@ -565,7 +565,19 @@
     ],
     "W9_DB_PORT_SET": [
         null,
-        "数据库端口"
+        "应用数据库端口"
     ]
-}));
+}));,
+    "W9_AGENT_PORT_SET": [
+        null,
+        "应用代理端口"
+    ],
+    "W9_SSH_PORT_SET": [
+        null,
+        "应用 SSH 端口"
+    ],
+    "W9_TCP_PORT_SET": [
+        null,
+        "应用 TCP 端口"
+    ]
 
