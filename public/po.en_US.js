830464c9e23b20978cc43b5cdfba774a1884fb86
qiaofeng1227 <qiaofeng1227@users.noreply.github.com>
2023-12-08 01:05:35 +0000

    Websoft9/plugin-myapps combine translation.json

--- public/po.en_US.js
+++ public/po.en_US.js
@@ -6,10 +6,30 @@ cockpit.locale({
     },
     "W9_HTTP_PORT_SET": [
         null,
-        "Application HTTP Port"
+        "App HTTP Port"
     ],
     "W9_DB_PORT_SET": [
         null,
         "Database Port"
+    ],
+    "W9_AGENT_PORT_SET": [
+        null,
+        "App Agent Port"
+    ],
+    "W9_SSH_PORT_SET": [
+        null,
+        "App SSH Port"
+    ],
+    "W9_TCP_PORT_SET": [
+        null,
+        "App TCP Port"
+    ],
+    "W9_API_PORT_SET": [
+        null,
+        "App API Port"
+    ],
+    "W9_DB_KAFKA_PORT_SET": [
+        null,
+        "KAFKA PORT"
     ]
 });
